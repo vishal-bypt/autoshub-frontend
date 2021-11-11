2fff3a63aa2fb87935143a8d1d95bb2401d44eee
Vishal Panchal <vishal.panchal@bypt.in>
2021-11-11 15:22:23 +0530

    Added the data export functonalities in all the dashboard graphs

--- src/pages/Dashboard/ManagerGraphs.js
+++ src/pages/Dashboard/ManagerGraphs.js
@@ -2,8 +2,70 @@ import React from "react";
 import { Col, Row, Card, CardBody, CardTitle, CardHeader } from "reactstrap";
 // import chartJs
 import EPieChart from "../../components/AllCharts/echart/piechart";
+import { Link } from "react-router-dom";
+import { dashboardService } from "../../services/dashboard.service";
+import Download from "@material-ui/icons/GetApp";
 
 const ManagerGraphs = (props) => {
+
+  let exportDataFormatForAssigned = [
+    {
+      keyName: "name",
+      lableName: "ToolName",
+      order: 1,
+    },
+    {
+      keyName: "value",
+      lableName: "Assigned",
+      order: 2,
+    },
+  ];
+
+  let exportDataFormatForNominated = [
+    {
+      keyName: "name",
+      lableName: "ToolName",
+      order: 1,
+    },
+    {
+      keyName: "value",
+      lableName: "Nominated",
+      order: 2,
+    },
+  ];
+
+  const handleDownloadDataForAssigned = () => {
+    dashboardService
+      .exportAnyReport(
+        "ExportAssignedTraining",
+        props.employeedWiseAssigned,
+        exportDataFormatForAssigned
+      )
+      .then((x) => {
+        const link = document.createElement("a");
+        link.href = x.exportPath;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
+  };
+
+  const handleDownloadDataForNominated = () => {
+    dashboardService
+      .exportAnyReport(
+        "ExportNominatedTraining",
+        props.employeedWiseNominated,
+        exportDataFormatForNominated
+      )
+      .then((x) => {
+        const link = document.createElement("a");
+        link.href = x.exportPath;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
+  };
+
   return (
     <React.Fragment>
       <Row>
@@ -19,6 +81,9 @@ const ManagerGraphs = (props) => {
                   <div className="card-body">
                   <Row className="justify-content-center text-center">
                     <h3 className="card-title">Assigned</h3>
+                    <Link to="#" onClick={handleDownloadDataForAssigned}>
+                          <Download title="Download" />
+                        </Link>
                   </Row>
                   <EPieChart title="Assigned" data={props.employeedWiseAssigned} />
                   </div>
@@ -29,6 +94,9 @@ const ManagerGraphs = (props) => {
                   <div className="card-body">
                   <Row className="justify-content-center text-center">
                     <h3 className="card-title">Nominated</h3>
+                    <Link to="#" onClick={handleDownloadDataForNominated}>
+                          <Download title="Download" />
+                        </Link>
                   </Row>
                   <EPieChart title="Nominated" data={props.employeedWiseNominated} />
                   </div>
